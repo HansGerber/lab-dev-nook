acc33a561cd02f7c8e38e24726c4b79756254f24
HansGerber <normal@Admin-PC>
2017-02-12 21:51:40 +0100

    add test audio file and adjust downloads page

--- views/downloads.php
+++ views/downloads.php
@@ -1,6 +1,17 @@
 <?php require_once "_header.php"; ?>
 <div class="contBox">
 	<h1>Downloads</h1>
-        No downloads yet.
+        <h3>(speed - Test File)</h3>
+        <div>
+            <audio controls>
+                <source src="<?php echo makePath("assets/audio/New6_9.MP3"); ?>">
+            </audio>
+        </div>
+        <h3>(speed - Test File 2)</h3>
+        <div>
+            <audio controls>
+                <source src="<?php echo makePath("assets/audio/New99.MP3"); ?>">
+            </audio>
+        </div>
 </div>
 <?php require_once "_footer.php"; ?>
