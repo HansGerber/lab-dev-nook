051b5fd706a8d09c898440a7314a4480bd77bf7e
HansGerber <normal@Admin-PC>
2017-02-19 00:17:01 +0100

    fix captcha data in conf template

--- conf_template.php
+++ conf_template.php
@@ -9,11 +9,11 @@
             ),
             "logPath" => "../logs/",
             "recaptcha" => array (
-                "dev" => array(
+                "live" => array(
                     "sitekey" => "6LeZxikTAAAAAAXBOXTnBqk6QS6sPNfDeIB5n1As",
                     "secret" => "6LeZxikTAAAAAKA35RjPFvtkjGb0tfow8qztwZRo"
                 ),
-                "live" => array (
+                "dev" => array (
                     "sitekey" => "6LdMUxgTAAAAAOXxiK4P7UFAD4nxepnjMNgySVQi",
                     "secret" => "6LdMUxgTAAAAAGq-ik1f5hfRxiajHSW-T_BD4eqf"
                 ),
