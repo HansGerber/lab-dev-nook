9393c4f3b68c23f7f8bc7a2d3162e3df27e385f6
HansGerber <normal@Admin-PC>
2017-02-18 17:50:57 +0100

    Add noscript notice

--- views/_header.php
+++ views/_header.php
@@ -5,6 +5,9 @@
 		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
 	<body>
+            <noscript>
+            Javascript needs to be enabled when using this site.<br>
+            </noscript>
             <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
