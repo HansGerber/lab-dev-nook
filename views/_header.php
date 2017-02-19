71f6ef55a35911e56d703b1e018dc2df8ad1470c
HansGerber <normal@Admin-PC>
2017-02-19 19:42:24 +0100

    Add adsense code

--- views/_header.php
+++ views/_header.php
@@ -29,3 +29,12 @@
                                 <a class="boxShadow <?php if(getPath() == "/links"){echo "active";} ?>" href="<?php echo makePath("links"); ?>">Links</a>
 			</div>
 			<div id="content" class="boxShadow">
+                            <div id="googleadsense">
+                                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <script>
+                                  (adsbygoogle = window.adsbygoogle || []).push({
+                                    google_ad_client: "ca-pub-1906246425506748",
+                                    enable_page_level_ads: true
+                                  });
+                                </script>
+                            </div>
