76681e69fd7029b401ffc2a8a5edac8d08b7a4ea
benjamin <benjamin@DESKTOP-9PE82MU>
2018-02-17 03:52:51 +0100

    Adjust python test script

--- web/test/ocr/tesseract/python-test/test1.py
+++ web/test/ocr/tesseract/python-test/test1.py
@@ -5,7 +5,8 @@
 import cgitb
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
+print "Content-Type: text/html;charset=utf-8"
 print
 
-print "Hello World!"
+print "<h1>Hello World!</h1>"
+print "<p>Lorem Ipsum dolor sit amet consetetur ...</p>"
