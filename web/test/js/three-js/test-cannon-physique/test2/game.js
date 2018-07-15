9c52312a72b67c333c8b0ea9780e5e948578a1f6
benjamin <benjamin@DESKTOP-9PE82MU>
2018-07-15 23:11:01 +0200

    Adjust cannon js example no. 2

--- web/test/js/three-js/test-cannon-physique/test2/game.js
+++ web/test/js/three-js/test-cannon-physique/test2/game.js
@@ -18,7 +18,65 @@ var game = window.game || {
 	renderer: null,
 	scene: null,
 	camera: null,
+	input: {
+		keysPressed: {
+			SPACE: false,
+			LEFT: false,
+			UP: false,
+			RIGHT: false,
+			DOWN: false
+		},
+		mouse: {
+			pos: {
+				x: 0,
+				y: 0
+			}
+		}
+	},
+	initialized: false,
 	init: function(config) {
+		addEventListener("keydown", (function(e){
+			switch(e.keyCode){
+				case 32:
+					this.input.keysPressed.SPACE = true;
+				break;
+				case 37:
+					this.input.keysPressed.LEFT = true;
+				break;
+				case 38:
+					this.input.keysPressed.UP = true;
+				break;
+				case 39:
+					this.input.keysPressed.RIGHT = true;
+				break;
+				case 40:
+					this.input.keysPressed.DOWN = true;
+				break;
+				default:
+			}
+		}).bind(this), false);
+
+		addEventListener("keyup", (function(e){
+			switch(e.keyCode){
+				case 32:
+					this.input.keysPressed.SPACE = false;
+				break;
+				case 37:
+					this.input.keysPressed.LEFT = false;
+				break;
+				case 38:
+					this.input.keysPressed.UP = false;
+				break;
+				case 39:
+					this.input.keysPressed.RIGHT = false;
+				break;
+				case 40:
+					this.input.keysPressed.DOWN = false;
+				break;
+				default:
+			}
+		}).bind(this), false);
+		
 		this.world = new CANNON.World();
 		this.world.broadphase = new CANNON.NaiveBroadphase();
 		this.world.solver.iterations = 10;
@@ -46,6 +104,12 @@ var game = window.game || {
 		this.scene.add(this.camera);
 		
 		this.render();
+		
+		this.initialized = true;
+		
+		if("renderLogic" in config && typeof config.renderLogic == "function"){
+			this.renderLogic = config.renderLogic.bind(this);
+		}
 		
 		if("after" in config && typeof config.after == "function"){
 			config.after.bind(this)();
@@ -101,6 +165,14 @@ var game = window.game || {
 			object.body = body;
 			object.mesh = mesh;
 			
+			object.canJump = (function() {
+				var raycaster = new THREE.Raycaster();
+                                // TODO
+			}).bind({
+				object: object,
+				game: this
+			})
+			
 			this.world.addBody(body);
 			this.scene.add(mesh);
 			
@@ -113,6 +185,17 @@ var game = window.game || {
 	removeObject: function(object) {
 		
 	},
+	on: function(eventName, fnCallback){
+		if(typeof fnCallback === "function"){
+			var handler = fnCallback.bind(this);
+			switch(eventName){
+				default:
+					addEventListener(eventName, handler, false);
+			}
+			return handler;
+		}
+		return false;
+	},
 	updatePhysics: function() {
 		this.world.step(this.config.phys.timeStep);
 		for(p in this.objects){
@@ -120,8 +203,14 @@ var game = window.game || {
 			this.objects[p].mesh.quaternion.copy(this.objects[p].body.quaternion);
 		}
 	},
+	renderLogic: null,
 	render: function() {
 		requestAnimationFrame(this.render.bind(this))
+		
+		if(typeof this.renderLogic === "function"){
+			(this.renderLogic.bind(this))();
+		}
+		
 		this.updatePhysics();
 		this.renderer.render(
 			this.scene,
--- web/test/js/three-js/test-cannon-physique/test2/index.php
+++ web/test/js/three-js/test-cannon-physique/test2/index.php
@@ -14,21 +14,60 @@
 		<script src="game.js"></script>
 		<script>
 		
+		var fallingObj, groundObj1, groundObj2, ground
+			moveSpeed = 0.1;
+		
+		function jump(){
+			// TODO: prevent when not on ground
+			fallingObj.body.velocity.y = 10;
+		}
+		
+		function moveForward(){
+			/*fallingObj.body.velocity.z = 0;
+			fallingObj.body.position.z -= moveSpeed;*/
+			fallingObj.body.velocity.z = -5;
+		}
+		
+		function moveRight(){
+			/*fallingObj.body.velocity.x = 0;
+			fallingObj.body.position.x += moveSpeed;*/
+			fallingObj.body.velocity.x = 5;
+		}
+		
+		function moveBackward(){
+			/*fallingObj.body.velocity.z = 0;
+			fallingObj.body.position.z += moveSpeed;*/
+			fallingObj.body.velocity.z = 5;
+		}
+		
+		function moveLeft(){
+			/*fallingObj.body.velocity.x = 0;
+			fallingObj.body.position.x -= moveSpeed;*/
+			fallingObj.body.velocity.x = -5;
+		}
+		function lockCameraToObject(object){
+			this.camera.position.x = object.body.position.x;
+			this.camera.position.y = object.body.position.y + 20;
+			this.camera.position.z = object.body.position.z + 20;
+			this.camera.rotation.x = -(Math.PI / 4);
+		}
 		
 		addEventListener("load", function() {
 			
 			game.init({
 				after:function() {
 					
-					this.camera.position.z = 20;
+					this.world.addEventListener("postStep", function() {
+						// console.log("postStep", game.world.contacts);
+					}, false);
 					
-					var fallingObj = this.addObject({
+					fallingObj = this.addObject({
 						name: 'fallling',
 						wireframe: true,
 						size: [2, 1, 2],
 					});
 					
-					var groundObj1 = this.addObject({
+					groundObj1 = this.addObject({
 						name: 'ground1',
 						mass: 0,
 						color: 0xff0000,
@@ -36,21 +75,48 @@
 						size: [1, 1, 1],
 					});
 					
-					var groundObj2 = this.addObject({
+					groundObj2 = this.addObject({
 						name: 'ground2',
 						mass: 0,
 						color: 0x0000ff,
 						wireframe: true,
-						size: [4, 0.5, 4],
+						size: [6, 0.5, 6],
 					});
 					
 					fallingObj.body.position.y = 10;
+					fallingObj.body.fixedRotation = true;
+					fallingObj.body.updateMassProperties();
+					fallingObj.body.addEventListener("collide", function(e) {
+						// console.log("object collided", e);
+					}, false);
+					
+					this.on("keydown", function(e) {
+						if(e.keyCode == 32){
+							jump();
+						}
+					});
 					
 					groundObj1.body.position.x = 1;
 					groundObj1.body.position.y = 5;
 					
-					groundObj2.body.position.y = -1;
-				}
+					groundObj2.body.position.y = -4;
+				},
+				renderLogic: function() {
+					if(this.input.keysPressed.LEFT){
+						moveLeft();
+					}
+					if(this.input.keysPressed.UP){
+						moveForward();
+					}
+					if(this.input.keysPressed.RIGHT){
+						moveRight();
+					}
+					if(this.input.keysPressed.DOWN){
+						moveBackward();
+					}
+					
+					(lockCameraToObject.bind(this, fallingObj))();
+				},
 			});
 			
 		}, false)
