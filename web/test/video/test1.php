8565bec2a29d45fe97a5b726b4374010a356beb1
benjamin <benjamin@DESKTOP-9PE82MU>
2019-03-30 20:41:17 +0100

    add video tests

---
+++ web/test/video/test1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Video Test</title>
+    </head>
+    <body>
+        <video controls>
+            <source type="video/mp4" src="https://www.w3schools.com/html/mov_bbb.mp4" />
+            <source type="video/ogg" src="https://www.w3schools.com/html/mov_bbb.ogg" />
+            To play videos this website requires support for the video tag however your browser does not offer the required support.<br />
+            Please change or update your browser in oder to watch this video.
+        </video>
+    </body>
+</html>
